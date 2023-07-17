c1a2acb086837187f574ac53a262f8a06b23e85f
harikrishnakreji <harikrishnakreji@gmail.com>
2023-07-17 16:12:07 +0530

    Second commit added Login with token

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/harikrishnakreji/GO-JWT/initializers"
 	"github.com/harikrishnakreji/GO-JWT/models"
 	"golang.org/x/crypto/bcrypt"
@@ -13,7 +16,7 @@ func Signup(c *gin.Context) {
 	//Get the Email/Password of Req Body
 	var body struct {
 		Email    string
-		password string
+		Password string
 	}
 	if c.Bind(&body) != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -22,7 +25,7 @@ func Signup(c *gin.Context) {
 		return
 	}
 	//Hash the Password
-	hash, err := bcrypt.GenerateFromPassword([]byte(body.password), 10)
+	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to hash password",
@@ -43,4 +46,59 @@ func Signup(c *gin.Context) {
 
 	//Respond
 	c.JSON(http.StatusBadRequest, gin.H{})
+
+}
+func Login(c *gin.Context) {
+	//Get the email and pass off req body
+	var body struct {
+		Email    string
+		Password string
+	}
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+		return
+	}
+	//Look up requested user
+	var user models.User
+	initializers.DB.User(&user, "email", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid email or password",
+		})
+		return
+	}
+
+	//comapre sent in pass with saved user pass hash
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid email or password",
+		})
+		return
+	}
+	//Generate a jwt token
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString(os.Getenv("SECRET"))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "FAIL TO CREATE TOKEN",
+		})
+		return
+	}
+	//send it back
+	c.JSON(http.StatusBadRequest, gin.H{
+		"token": tokenString,
+	})
+
 }
